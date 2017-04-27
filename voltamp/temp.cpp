0652328287b45c1341229c1eba9ff93653fdf181
Tsukihime <vovan7773@gmail.com>
2017-04-27 15:53:54 +0300

    Change pinout for new PCB

--- voltamp/temp.cpp
+++ voltamp/temp.cpp
@@ -1,5 +1,6 @@
 #include "temp.h"
 #include "DS18B20_defs.h"
+#include "config.h"
 
 extern "C" {
 #include "lib/1-wire-avr-lib/OWIPolled.h"
@@ -8,36 +9,34 @@ extern "C" {
 #include "lib/1-wire-avr-lib/common_files/OWIcrc.h"
 }
 
-#define BUS OWI_PIN_5
-
 TemperatureSensor temperatureSensor;
 
 void TemperatureSensor::initialize() {
-    OWI_Init(BUS);
-    OWI_SendByte(DS18B20_WRITE_SCRATCHPAD, BUS);
+    OWI_Init(TEMP_PIN);
+    OWI_SendByte(DS18B20_WRITE_SCRATCHPAD, TEMP_PIN);
 
-    OWI_SendByte(0, BUS);
-    OWI_SendByte(0, BUS);
-    OWI_SendByte(DS18B20_RES_12BIT, BUS);
+    OWI_SendByte(0, TEMP_PIN);
+    OWI_SendByte(0, TEMP_PIN);
+    OWI_SendByte(DS18B20_RES_12BIT, TEMP_PIN);
 }
 
 void TemperatureSensor::startMeasurement() {
-    OWI_Init(BUS);
-    OWI_DetectPresence(BUS);
-    OWI_SkipRom(BUS);
-    OWI_SendByte(DS18B20_CONVERT_T, BUS);
+    OWI_Init(TEMP_PIN);
+    OWI_DetectPresence(TEMP_PIN);
+    OWI_SkipRom(TEMP_PIN);
+    OWI_SendByte(DS18B20_CONVERT_T, TEMP_PIN);
 }
 
 int16_t TemperatureSensor::readRawTemperature() {    
     /*wait while semsor complete measurement*/
     //while (!OWI_ReadBit(BUS));
 
-    OWI_DetectPresence(BUS);
-    OWI_SkipRom(BUS);
-    OWI_SendByte(DS18B20_READ_SCRATCHPAD, BUS);
+    OWI_DetectPresence(TEMP_PIN);
+    OWI_SkipRom(TEMP_PIN);
+    OWI_SendByte(DS18B20_READ_SCRATCHPAD, TEMP_PIN);
 
-    uint8_t LS = OWI_ReceiveByte(BUS);
-    uint8_t MS = OWI_ReceiveByte(BUS);
+    uint8_t LS = OWI_ReceiveByte(TEMP_PIN);
+    uint8_t MS = OWI_ReceiveByte(TEMP_PIN);
 
     int16_t data = ((MS << 8) | LS);
     return data;
