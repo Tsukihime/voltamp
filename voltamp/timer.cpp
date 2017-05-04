90a4e565d5a6710df6863d18baeba6ad252a9289
Tsukihime <vovan7773@gmail.com>
2017-05-04 03:34:12 +0300

    Refactoring

--- voltamp/timer.cpp
+++ voltamp/timer.cpp
@@ -10,12 +10,12 @@ ISR(TIMER2_COMP_vect) {
 }
 
 #define TIMER2_PRESCALER_DIV64 ((1 << CS22) | (0 << CS21) | (0 << CS20))
-#define TIMER_CTC_MODE ((1 << WGM21) | (0 << WGM20))
+#define TIMER2_CTC_MODE ((1 << WGM21) | (0 << WGM20))
 #define COMPARE_RESET_DIV64_1MS_VALUE ((F_CPU / 1000 / 64) - 1)
 
 void Timer::initialize() {
     taskCount = 0;
-    TCCR2 = TIMER2_PRESCALER_DIV64 | TIMER_CTC_MODE;
+    TCCR2 = TIMER2_PRESCALER_DIV64 | TIMER2_CTC_MODE;
     OCR2 = COMPARE_RESET_DIV64_1MS_VALUE;
     TCNT2 = 0;
     enableInterrupts();
